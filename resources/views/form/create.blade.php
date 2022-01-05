ee0eea2574e74696a6854be186d7ea4c37c0af77
Artwewe <artempolushin@hotmail.com>
2022-01-05 22:08:46 +0000

    Added main view and form create view

---
+++ resources/views/form/create.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="formContainer h-screen bg-purple-100 w-1/2 m-auto">
+
+    <div class="formInfo bg-white w-[90%] p-4 mt-4 space-y-4 rounded m-auto">
+        <div>
+            <input type="text" placeholder="Form title" class="w-full rounded border border-black">
+        </div>
+
+        <div>
+            <input type="text" placeholder="Form description" class="w-full rounded border border-black">
+        </div>
+    </div>
+
+    <div class="questionContainer bg-white w-[90%] m-auto rounded mt-4 p-4">
+
+        <div class="questionInfo grid grid-cols-2">
+            <div>
+                <input type="text" placeholder="Question" class="w-full rounded border border-black">
+            </div>
+            <div class="flex justify-center items center">
+                <select name="questionType" class="rounded border border-black">
+                    <option value="multiple">Multiple Choice</option>
+                    <option value="checkbox">Checkbox</option>
+                    <option value="dropdown">Dropdown</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="questionOptions grid grid-cols-1 space-y-4 mt-4">
+            <div class="question grid grid-cols-2">
+                <div class="questionDescription">
+                    Option 1
+                </div>
+
+                <div class="deleteIcon text-right">
+                    delete
+                </div>
+            </div>
+
+            <div class="question grid grid-cols-2">
+                <div class="questionDescription">
+                    Option 2
+                </div>
+
+                <div class="deleteIcon text-right">
+                    delete
+                </div>
+            </div>
+
+            <div class="question grid grid-cols-2">
+                <div class="questionDescription">
+                    Option 3
+                </div>
+
+                <div class="deleteIcon text-right">
+                    delete
+                </div>
+            </div>
+
+            <div class="addOptionContainer flex justify-center items-center mt-4">
+                <div class="w-12 h-12 rounded-full flex justify-center items-center cursor-pointer bg-green-500">
+                    add
+                </div>
+            </div>
+        </div>
+
+        <hr class="mt-4">
+
+        <div class="questionSettings grid grid-cols-3 w-full">
+
+            <div class="col-span-2 flex space-x-4 justify-end mr-4">
+                <div>duplicar</div>
+                <div>apagar</div>
+            </div>
+
+            <div class="flex space-x-2 pl-4 border-l-[1px]">
+                <div>Required</div>
+                <div>button</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="addQuestionContainer flex justify-center items-center mt-4">
+        <div class="w-12 h-12 rounded-full flex justify-center items-center cursor-pointer bg-green-500">
+            add
+        </div>
+    </div>
+
+
+</div>
+
+
+@endsection
